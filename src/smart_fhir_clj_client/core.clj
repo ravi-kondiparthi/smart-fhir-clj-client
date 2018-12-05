6b72dbc86c66bd7d0431daf78ab1e51f07928f91
Palanivel <r633477@TRG529590.local>
2018-12-05 10:28:54 -0800

    Clojre Project Structure added

---
+++ src/smart_fhir_clj_client/core.clj
@@ -0,0 +1,6 @@
+(ns smart-fhir-clj-client.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/smart_fhir_clj_client/core_test.clj
@@ -0,0 +1,7 @@
+(ns smart-fhir-clj-client.core-test
+  (:require [clojure.test :refer :all]
+            [smart-fhir-clj-client.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
