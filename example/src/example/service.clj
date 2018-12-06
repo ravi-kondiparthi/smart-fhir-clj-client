0b72348ad8fcaf521a54f55a31c1bb4a3511ab29
ravi kondiparthi <ravindra.kondiparthi@cambiahealth.com>
2018-12-06 11:31:44 -0800

    select resource html page

--- example/src/example/service.clj
+++ example/src/example/service.clj
@@ -3,22 +3,38 @@
             [io.pedestal.http.route :as route]
             [io.pedestal.http.body-params :as body-params]
             [cheshire.core :as json]
+<<<<<<< HEAD
             [ring.util.response :as ring-resp]
             [clostache.parser :as clostache]
             [clojure.tools.logging :as log]
             [smart-fhir-clj-client.fhir :as sfcc]))
+=======
+            [net.cgrand.enlive-html :as html]
+            [clojure.java.io :as io]
+            [ring.util.response :as ring-resp]))
+;; Define the template
+>>>>>>> select resource html page
 
+
+
+(def data-file (io/resource
+                 "select_resource.html" ))
 
 (defn token
   []
   {:name ::token
    :enter (fn [context]
-            (let [params (get-in context [:request :query-params])]
+            (let [params (get-in context [:request :query-params])
+                  html-data (slurp data-file)
+                  replace-data-token (clojure.string/replace html-data #"REPLACE_TOKEN" "valid-token")
+                  replace-data-patient (clojure.string/replace replace-data-token #"REPLACE_PATIENT_ID" "valid-patienid")
+                  replace-data-client-id (clojure.string/replace replace-data-token #"REPLACE_CLIENT_ID"
+                                                                "valid-clientid")
+                  ]
               (println params)
-              ; TODO next step is to exchange auth code for token
-              (assoc context :response {:body (json/encode params)
-                                        :headers {"Content-Type" "application/json"}
-                                        :status  200})))})
+
+              (assoc context :response {:status 200 :body replace-data
+                                        :headers {"Content-Type" "text/html"}})))})
 
 
 (defn init
@@ -26,7 +42,9 @@
   {:name ::init
    :enter (fn [context]
             (assoc context :response (-> (ring-resp/resource-response "index.html" {:root "public"})
-                                         (ring-resp/content-type "text/html"))))})
+                                         (ring-resp/content-type "text/html")
+                                          )))})
+
 
 (defn authorize
       []
@@ -52,6 +70,8 @@
               ["/epic/token/demo" :get (conj common-interceptors (token))]})
 
 
+
+;;;TODO add intereceptor to initialize app when calls are received on token on
 
 ;; Consumed by example.server/create-server
 ;; See http/default-interceptors for additional options you can configure
